1817548b67bdd847d7d989c7568305533b0d64df
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-11 11:05:15 +0100

    Creating Your First EJS Templates

--- app.js
+++ app.js
@@ -10,18 +10,44 @@ const bodyParser = require('body-parser') // Add Body Parser
 // 3 - Create app constant using express
 const app = express()
 
+// 6 - Setup ejs, placed below app declared
+app.set('view engine', 'ejs')
+
 // 5 - Creating the first GET route
 app.get('/', function (req, res) {
 	// user access the Home route
 	var today = new Date()
 	var currentDay = today.getDay()
+	var day = ''
 
-	if (currentDay === 6 || currentDay === 0) {
-		res.write("<h1>Yay it's the weekend</h1>")
-	} else {
-		// response from server side
-		res.sendFile(__dirname + '/index.html')
+	switch (currentDay) {
+		case 0:
+			day = 'Sunday'
+			break
+		case 1:
+			day = 'Montag'
+			break
+		case 2:
+			day = 'Dienstag'
+			break
+		case 3:
+			day = 'Mittwoch'
+			break
+		case 4:
+			day = 'Donnerstag'
+			break
+		case 5:
+			day = 'Freitag'
+			break
+		case 6:
+			day = 'Samstag'
+			break
+
+		default:
+			console.log('Error: current day is equal to: ' + currentDay)
 	}
+
+	res.render('list', { kindOfDay: day })
 })
 
 // 4 - Write App listen in port 3000
